4dde839ce3c737a0a0eaee36784115df0258ccbc
manishshettym <manishs@berkeley.edu>
2024-10-03 14:02:18 -0700

    fix: flip filter logic

--- r2e/repo_builder/fut_extractor/extract_functions.py
+++ r2e/repo_builder/fut_extractor/extract_functions.py
@@ -12,19 +12,19 @@ class FileFunctionExtractor(FileBaseExtractor):
     ) -> list[ast.FunctionDef]:
         function_asts = FileFunctionExtractor.get_functions_from_ast(astree)
 
-        if not repo_args.disable_all_filters:
+        if repo_args.disable_all_filters:
             return function_asts
 
         ## remove dunder methods
-        if not repo_args.disable_dunder_methods:
+        if repo_args.disable_dunder_methods:
             function_asts = FileFunctionExtractor.filter_dunder_methods(function_asts)
 
         ## remove functions without docstrings
-        if not repo_args.disable_no_docstring:
+        if repo_args.disable_no_docstring:
             function_asts = FileFunctionExtractor.filter_keep_docstring(function_asts)
 
         ## remove functions without arguments, returns | with literal returns
-        if not repo_args.disable_signature_filters:
+        if repo_args.disable_signature_filters:
             function_asts = FileFunctionExtractor.filter_nonzero_arguments(
                 function_asts
             )
@@ -32,7 +32,7 @@ class FileFunctionExtractor(FileBaseExtractor):
             function_asts = FileFunctionExtractor.filter_literal_returns(function_asts)
 
         ## keyword filters and bad function names
-        if not repo_args.disable_keyword_filters:
+        if repo_args.disable_keyword_filters:
             function_asts = FileFunctionExtractor.filter_docstring_keywords(
                 function_asts
             )
@@ -44,7 +44,7 @@ class FileFunctionExtractor(FileBaseExtractor):
             )
 
         ## remove wrapper and decorated methods
-        if not repo_args.disable_wrapper_filters:
+        if repo_args.disable_wrapper_filters:
             function_asts = FileFunctionExtractor.filter_with_decorator(function_asts)
             function_asts = FileFunctionExtractor.filter_onlt_one_stmt(function_asts)
 
