b90727267e2bffb5c78ca522d6eed153d78bc9ae
manishshettym <manishs@berkeley.edu>
2024-10-10 19:25:45 -0700

    feat(cli): show execution summary

--- src/r2e/cli.py
+++ src/r2e/cli.py
@@ -10,6 +10,7 @@ from r2e.repo_builder import RepoArgs, SetupRepos, build_functions_and_methods, 
 from r2e.generators.testgen import TestGenArgs, R2ETestGenerator, GenExecArgs, R2EGenExec
 from r2e.execution.args import ExecutionArgs
 from r2e.execution.execute import EquivalenceTestRunner
+from r2e.evaluators.testgen import summarize
 
 from r2e.utils.data import load_functions, load_functions_under_test
 from r2e.models import *
@@ -225,103 +226,6 @@ def list_functions(exp_id, detailed, limit):
 
     if not detailed:
         click.echo("\nUse --detailed or -d for more information about each function.")
-
-
-################### r2e show ###################
-
-@r2e.command()
-@click.option('--exp_id', '-e', default="temp", help="Experiment ID used for prefixing the extracted functions file")
-@click.option('--fname', '-f', required=True, help="Name of the function to show.")
-@click.option('--show-code', '-c', is_flag=True, help="Show the code of the function.")
-@click.option('--show-test', '-t', is_flag=True, help="Show the generated test for the function.")
-@click.option('--show-result', '-r', is_flag=True, help="Show the generated test for the function.")
-@click.option('--show-chat', is_flag=True, help="Show the chat messages leading up to the final test for the function.")
-@click.option('--show-all', '-a', is_flag=True, help="Show both the code and the generated test for the function.")
-def show(exp_id, fname, show_code, show_test, show_result, show_chat, show_all):
-    """Show detailed information about a specific function."""
-    extracted_file_path = os.path.join(EXTRACTED_DATA_DIR, f"{exp_id}_extracted.json")
-    
-    if not os.path.exists(extracted_file_path):
-        click.echo(f"No extracted functions found for experiment ID: {exp_id}")
-        return
-
-    functions = load_functions(extracted_file_path)
-    target_function = next((func for func in functions if func.name == fname), None)
-    if not target_function:
-        click.echo(f"No function named '{fname}' found in the extracted data.")
-        return
-    
-    full_name, signature, docstring, ftype = get_func_info(target_function)
-
-    click.echo(f"{ftype}: {full_name}")
-    click.echo(f"File: {target_function.file.file_path}")
-    
-    if show_code or show_all:
-        click.echo("Code:\n")
-        click.echo(textwrap.indent(target_function.code, '    '))
-
-    testgen_file_path = os.path.join(TESTGEN_DIR, f"{exp_id}_generate.json")
-    executed_file_path = os.path.join(EXECUTION_DIR, f"{exp_id}_out.json")
-
-    if show_test or show_all:
-        if not os.path.exists(testgen_file_path) or not os.path.exists(executed_file_path):
-            click.echo(f"\nNo generated tests found for experiment ID: {exp_id}")
-            return
-
-        if os.path.exists(executed_file_path):
-            functions_under_test = load_functions_under_test(executed_file_path)
-        else:
-            functions_under_test = load_functions_under_test(testgen_file_path)
-            
-        target_fut = next((fut for fut in functions_under_test if fut.name == fname), None)
-        
-        if not target_fut:
-            click.echo(f"\nNo generated test found for function '{fname}'.")
-            return
-
-        click.echo("\nGenerated Test:")
-
-        for test_name, test_code in target_fut.test_history.latest_tests.items():
-            click.echo(f"\n{test_name}:")
-            click.echo(textwrap.indent(test_code, '    '))
-        
-    if show_result or show_all:
-        if not os.path.exists(executed_file_path):
-            click.echo(f"\nNo executed tests found for experiment ID: {exp_id}")
-            return
-
-        executed_futs = load_functions_under_test(executed_file_path)
-        target_executed_fut = next((fut for fut in executed_futs if fut.name == fname), None)
-        
-        if not target_executed_fut:
-            click.echo(f"\nNo executed test found for function '{fname}'.")
-            return
-
-        click.echo("\nTest Results:")
-
-        for test_name, results in target_executed_fut.exec_stats['run_tests_logs'].items():
-            click.echo(f"\n{test_name}:")
-            click.echo(json.dumps(results, indent=4))
-            click.echo(json.dumps(target_executed_fut.coverage, indent=4))
-    
-    if show_chat or show_all:
-        if not os.path.exists(executed_file_path):
-            click.echo(f"\nNo executed tests found for experiment ID: {exp_id}")
-            return
-
-        executed_futs = load_functions_under_test(executed_file_path)
-        target_executed_fut = next((fut for fut in executed_futs if fut.name == fname), None)
-        
-        if not target_executed_fut:
-            click.echo(f"\nNo executed test found for function '{fname}'.")
-            return
-
-        click.echo("\nChat Messages:")
-        
-        for chat_messages in target_executed_fut.test_history.latest_chat_messages:
-            click.secho(f"{chat_messages['role'].capitalize()}:", fg="green")
-            truncated_content = '\n'.join(chat_messages['content'].split('\n')[:50])
-            click.echo(truncated_content)
                     
 
 ################### r2e execute ###################
@@ -406,6 +310,115 @@ def genexec(**kwargs):
     args = GenExecArgs(**kwargs)
     EXECUTION_DIR.mkdir(parents=True, exist_ok=True)
     R2EGenExec.genexec(args)
+    
+
+################### r2e show ###################
+
+@r2e.command()
+@click.option('--exp_id', '-e', default="temp", help="Experiment ID used for prefixing the extracted functions file")
+@click.option('--fname', '-f', required=False, help="Name of the function to show.")
+@click.option('--code', '-c', is_flag=True, help="Show the code of the function.")
+@click.option('--test', '-t', is_flag=True, help="Show the generated test for the function.")
+@click.option('--result', '-r', is_flag=True, help="Show the generated test for the function.")
+@click.option('--show-all', '-a', is_flag=True, help="Show (code, test, result) for function(s).")
+# additional options
+@click.option('--chat', is_flag=True, help="Show the chat messages leading up to the final test for a function.")
+@click.option('--summary', is_flag=True, help="Show an overall summary of the test execution results.")
+def show(exp_id, fname, code, test, result, show_all, chat, summary):
+    """Show detailed information about a specific function."""
+    if not summary and not fname:
+        click.secho("Error: Please provide the name of the function using --fname.", fg="red")
+        return
+    
+    extracted_file_path = os.path.join(EXTRACTED_DATA_DIR, f"{exp_id}_extracted.json")
+    testgen_file_path = os.path.join(TESTGEN_DIR, f"{exp_id}_generate.json")
+    executed_file_path = os.path.join(EXECUTION_DIR, f"{exp_id}_out.json")
+    
+    if summary:
+        if not os.path.exists(executed_file_path):
+            click.echo(f"\nNo executed tests found for experiment ID: {exp_id}")
+            return
+
+        click.echo(summarize(exp_id))
+        return
+    
+    if not os.path.exists(extracted_file_path):
+        click.echo(f"No extracted functions found for experiment ID: {exp_id}")
+        return
+
+    functions = load_functions(extracted_file_path)
+    target_function = next((func for func in functions if func.name == fname), None)
+    if not target_function:
+        click.echo(f"No function named '{fname}' found in the extracted data.")
+        return
+    
+    full_name, signature, docstring, ftype = get_func_info(target_function)
+    click.echo(f"{ftype}: {full_name}")
+    click.echo(f"File: {target_function.file.file_path}")
+    
+    if code or show_all:
+        click.echo("Code:\n")
+        click.echo(textwrap.indent(target_function.code, '    '))
+    
+    if test or show_all:
+        if not os.path.exists(testgen_file_path) or not os.path.exists(executed_file_path):
+            click.echo(f"\nNo generated tests found for experiment ID: {exp_id}")
+            return
+
+        if os.path.exists(executed_file_path):
+            functions_under_test = load_functions_under_test(executed_file_path)
+        else:
+            functions_under_test = load_functions_under_test(testgen_file_path)
+            
+        target_fut = next((fut for fut in functions_under_test if fut.name == fname), None)
+        
+        if not target_fut:
+            click.echo(f"\nNo generated test found for function '{fname}'.")
+            return
+
+        click.echo("\nGenerated Test:")
+
+        for test_name, test_code in target_fut.test_history.latest_tests.items():
+            click.echo(f"\n{test_name}:")
+            click.echo(textwrap.indent(test_code, '    '))
+        
+    if result or show_all:
+        if not os.path.exists(executed_file_path):
+            click.echo(f"\nNo executed tests found for experiment ID: {exp_id}")
+            return
+
+        executed_futs = load_functions_under_test(executed_file_path)
+        target_executed_fut = next((fut for fut in executed_futs if fut.name == fname), None)
+        
+        if not target_executed_fut:
+            click.echo(f"\nNo executed test found for function '{fname}'.")
+            return
+
+        click.echo("\nTest Results:")
+
+        for test_name, results in target_executed_fut.exec_stats['run_tests_logs'].items():
+            click.echo(f"\n{test_name}:")
+            click.echo(json.dumps(results, indent=4))
+            click.echo(json.dumps(target_executed_fut.coverage, indent=4))
+    
+    if chat or show_all:
+        if not os.path.exists(executed_file_path):
+            click.echo(f"\nNo executed tests found for experiment ID: {exp_id}")
+            return
+
+        executed_futs = load_functions_under_test(executed_file_path)
+        target_executed_fut = next((fut for fut in executed_futs if fut.name == fname), None)
+        
+        if not target_executed_fut:
+            click.echo(f"\nNo executed test found for function '{fname}'.")
+            return
+
+        click.echo("\nChat Messages:")
+        
+        for chat_messages in target_executed_fut.test_history.latest_chat_messages:
+            click.secho(f"{chat_messages['role'].capitalize()}:", fg="green")
+            truncated_content = '\n'.join(chat_messages['content'].split('\n')[:50])
+            click.echo(truncated_content)
 
 if __name__ == '__main__':
     r2e()
